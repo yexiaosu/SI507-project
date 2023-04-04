bf97b45805e36404137abf9f2eb2f109f8c241f6
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-04-04 17:02:04 -0400

    fix: divide to two files

--- cards_data_collection.py
+++ cards_data_collection.py
@@ -3,38 +3,9 @@ import requests
 import json
 import logging
 from cache import saveCache, openCache
+from oauthToken import getNewToken
 
 CACHE_FILENAME = "cards.json"
-
-def getNewToken():
-    ''' get a new OAuth 2.0 token from the authentication server
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    string
-        the token
-    '''
-    authServerUrl = "https://oauth.battle.net/token"
-    clientId = '2d49b07cd937443ca11518271989ed4d'
-    clientSecret = 'cLeBHmV1yQPK9i6g8g13Iq5fxq6drTIT'
-
-    tokenReqPayload = {'grant_type': 'client_credentials'}
-
-    tokenResponse = requests.post(authServerUrl,
-                                  data=tokenReqPayload, verify=False, allow_redirects=False,
-                                  auth=(clientId, clientSecret))
-
-    if tokenResponse.status_code != 200:
-        print("Failed to obtain token from the OAuth 2.0 server", file=sys.stderr)
-        sys.exit(1)
-    print("Successfuly obtained a new token")
-    tokens = json.loads(tokenResponse.text)
-    return tokens['access_token']
-
 
 def main():
     ''' get and save data of cards
@@ -59,7 +30,8 @@ def request(apiUrl):
 
     Parameters
     ----------
-    None
+    apiUrl: string
+        URL of the card API
 
     Returns
     -------
