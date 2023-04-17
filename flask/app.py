7feca584f3eb73aad9a4a8432a49eff4b762ca42
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-04-17 14:27:23 -0500

    feat: search deck

--- flask/app.py
+++ flask/app.py
@@ -9,8 +9,31 @@ def index():
     return render_template('index.html')
 
 @app.route('/decks')
-def decks():
-    return render_template('index.html')
+def find_decks():
+    formats = ["wild", "hydra", "mammoth", "classic"]
+    classes = ["Warrior", "Warlock", "Shaman", "Rogue", "Priest", "Paladin", "Mage", "Hunter", "Druid", "Demon Hunter", "Death Knight"]
+    return render_template('search_decks.html', formats = formats, classes = classes)
+
+@app.route('/decks/<formats>/<classes>')
+def show_decks(formats, classes):
+    with open('data/decks_tree.json') as json_file:
+        decks = json.load(json_file)
+    print(decks)
+    selected_decks = []
+    if formats == "all":
+        for format in decks:
+            if classes == "all":
+                for class_name in decks[format]:
+                    selected_decks.extend(decks[format][class_name])
+            else:
+                selected_decks.extend(decks[format][classes])
+    else:
+        if classes == "all":
+            for class_name in decks[formats]:
+                selected_decks.extend(decks[formats][class_name])
+        else:
+            selected_decks.extend(decks[formats][classes])
+    return render_template('decks.html', decks=selected_decks)
 
 @app.route('/cards_data')
 def get_cards_data():
---
+++ flask/static/js/deck.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    let current_fs, next_fs, previous_fs; //fieldsets
+    let opacity;
+    let current = 1;
+    let steps = $("fieldset").length;
+    window.format = "all";
+    window.class = "all";
+
+    setProgressBar(current);
+
+    $(".inputGroup").click(function (e) {
+        if (e.target.tagName === "INPUT") {
+            if (e.target.name === "format") {
+                window.format = e.target.value;
+            }
+            if (e.target.name === "class") {
+                window.class = e.target.value;
+            }
+        }
+    })
+
+    $(".next").click(function () {
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate(
+            { opacity: 0 },
+            {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        display: "none",
+                        position: "relative",
+                    });
+                    next_fs.css({ opacity: opacity });
+                },
+                duration: 500,
+            }
+        );
+        setProgressBar(++current);
+    });
+
+    $(".previous").click(function () {
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+        //show the previous fieldset
+        previous_fs.show();
+
+        //hide the current fieldset with style
+        current_fs.animate(
+            { opacity: 0 },
+            {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        display: "none",
+                        position: "relative",
+                    });
+                    previous_fs.css({ opacity: opacity });
+                },
+                duration: 500,
+            }
+        );
+        setProgressBar(--current);
+    });
+
+    function setProgressBar(curStep) {
+        var percent = parseFloat(100 / steps) * curStep;
+        percent = percent.toFixed();
+        $(".progress-bar").css("width", percent + "%");
+    }
+
+    $(".submit").click(function () {
+        return false;
+    });
+});
