b501180ffd117454b403ea0486768578faac5687
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-04-18 16:35:45 -0500

    feat: cards list

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, jsonify
 import json
+from operator import itemgetter
 
 app = Flask(__name__)
 
@@ -56,6 +57,43 @@ def find_cards():
     types = ["Hero", "Minion", "Spell", "Weapon", "HeroPower", "Location", "Reward"]
     rarities = ["Common", "Free", "Rare", "Epic", "Legendary"]
     return render_template('search_cards.html', classes = classes, types = types, rarities = rarities)
+
+@app.route('/cards/<classes>/<types>/<rarities>')
+def show_cards(classes, types, rarities):
+    with open('data/cards_tree.json') as json_file:
+        cards = json.load(json_file)
+    selected_cards = []
+    if classes == "all":
+        for class_name in cards:
+            if types == "all":
+                for type in cards[class_name]:
+                    if rarities == "all":
+                        for rarity in cards[class_name][type]:
+                            selected_cards.extend(cards[class_name][type][rarity])
+                    else:
+                        selected_cards.extend(cards[class_name][type][rarities])
+            else:
+                if rarities == "all":
+                    for rarity in cards[class_name][type]:
+                        selected_cards.extend(cards[class_name][type][rarity])
+                else:
+                    selected_cards.extend(cards[class_name][type][rarities])
+    else:
+        if types == "all":
+            for type in cards[classes]:
+                if rarities == "all":
+                    for rarity in cards[classes][type]:
+                        selected_cards.extend(cards[classes][type][rarity])
+                else:
+                    selected_cards.extend(cards[classes][type][rarities])
+        else:
+            if rarities == "all":
+                for rarity in cards[classes][type]:
+                    selected_cards.extend(cards[classes][type][rarity])
+            else:
+                selected_cards.extend(cards[classes][type][rarities])
+    selected_cards_sorted = sorted(selected_cards, key=itemgetter('popular'), reverse=True)
+    return render_template('cards.html', cards=selected_cards_sorted, class_name = classes, type = types, rarity = rarities)
 
 @app.route('/cards_data')
 def get_cards_data():
